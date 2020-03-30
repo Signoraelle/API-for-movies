ce5b605f8f40ae4d4af81b77d2be265e151bea12
Signoraelle <alicecasadei@icloud.com>
2020-03-30 09:18:14 +0200

    up5

--- movie.js
+++ movie.js
@@ -6,11 +6,33 @@ app.get("/api/MOVIE", (req, res) => {
   // connection to the database, and selection of movie
   connection.query("SELECT * FROM MOVIE", (err, results) => {
     if (err) {
-      
       //  If an error has occurred, then the user is informed of the error
       res.status(500).send("Error retrieving all movies");
     } else {
       // If everything went well, we send the result of the SQL query as JSON.
+      res.json(results);
+    }
+  });
+});
+
+app.listen(3000, err => {
+  if (err) {
+    throw new Error("Alert alert");
+  }
+  console.log("Listening on port 3000");
+});
+
+// All movies name
+
+const express = require("express");
+const app = express();
+const connection = require("./config");
+
+app.get("/api/MOVIE", (req, res) => {
+  connection.query("SELECT name FROM MOVIE", (err, results) => {
+    if (err) {
+      res.status(500).send("Error retrieving names from movies");
+    } else {
       res.json(results);
     }
   });
